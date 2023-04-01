8385b2a33d0be7f34151ef4ebd91bc3e8f6f6181
NicollyMeireles <nicollymeireles32@gmail.com>
2023-03-31 21:20:51 -0300

    ImitandoDory

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("--- IMITANDO DORY ---");
+Console.WriteLine("digite algo:______");
+string frase = Console.ReadLine()!;
+
+string fraseEmBaleies = frase
+    .Replace("a", "aaaa")
+    .Replace("A", "AAAA")
+    .Replace("e", "eeee")
+    .Replace("E", "EEEE")
+    .Replace("i", "iiii")
+    .Replace("I", "IIII")
+    .Replace("o", "oooo")
+    .Replace("O", "OOOO")
+    .Replace("u", "uuuu")
+    .Replace("U", "UUUU")
+    ;
+
+    Console.WriteLine($"\nEm baleiês:\n\n{fraseEmBaleies}");
+
